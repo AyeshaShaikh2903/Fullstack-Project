aba897fe2ee3f9e37244fd14ae751e8fc2190c36
Aayesha <ayeshashaikh2922@gmail.com>
2025-01-22 18:27:24 +0530

    comepleted the products component successfully

--- frontend/src/landing_page/products/ProductPage.js
+++ frontend/src/landing_page/products/ProductPage.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import Hero from '../about/Hero';
-import LeftSection from '../products/LeftSection';
-
-import RightSection from '../products/RightSection';
-import Universe from '../products/Universe';
-function ProductsPage() {
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
+function ProductPage() {
     return ( 
         <>
         <Hero/>
-        <LeftSection/>
+        <LeftSection 
+        imageURL="media\images\kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo="" 
+        learnMore="" 
+        googlePlay=""
+        appStore="" 
+        />
+         <RightSection
+         imageURL="media\images\console.png"
+         productName="Console"
+         productDescription="The central dashboard for your Zerodha account.Gain insights into your trades and investments with in-depth reports and visualizations."
+         learnMore="" 
+         />
+         <LeftSection 
+        imageURL="media\images\coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online,commission-free,delivered directly to your Demat account.Enjoy the investment experience on your Android and IOS devices."
+        tryDemo="" 
+        learnMore="" 
+        googlePlay=""
+        appStore="" 
+        />
+        <RightSection
+        imageURL="media\images\kiteconnect.png"
+        productName="Kite Connect API"
+        productDescription="Build powerful trading platforms with our super simple HTTP/PUSON APIs.if you are a startup ,build your investment app and showcase it to our baseclients"
+        learnMore="" 
+        />
+        <LeftSection 
+        imageURL="media\images\varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp,collection of stock market lessons with in depth coverage and illustrations.content is broken down into bite size cards to help you learn on the go."
+        tryDemo="" 
+        learnMore="" 
+        googlePlay=""
+        appStore="" 
+        />
+        <p className='text-center'>want to know more about our technology stack? check out the Zerodha .tech.blog</p>
         <RightSection/>
         <Universe/>
         </>
      );
 }
 
-export default ProductsPage ;
+export default ProductPage;
--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -1,7 +1,36 @@
 import React from 'react';
     function Universe() {
         return ( 
-            <h1>Universe</h1>
+            <div className='container'>
+            <div className='row text-center'>
+                <h1>The Zerodha Universe</h1>
+                <p>Extend your trading and investment experience even further with our partner platforms</p>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/smallcaseLogo.png'></img>
+                <p className='text-small text-muted'>Thematic investment platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/streakLogo.png' style={{width:"50%"}}></img>
+                <p className='text-small text-muted'>Algo & strategy platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/sensibullLogo.svg'></img>
+                <p className='text-small text-muted'>Options trading platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/zerodhaFundhouse.png' style={{width:"50%"}}></img>
+                <p className='text-small text-muted'>Asset management</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/goldenpiLogo.png'></img>
+                <p className='text-small text-muted'>Bonds trading platform</p>
+                </div>
+                <div className='col-4 p-3 mt-5'>
+                <img src='media/images/dittoLogo.png' style={{width:"30%"}}></img>
+                <p className='text-small text-muted'>Insure platform</p>
+                </div>
+            </div>
+        </div>
          );
     }
     
